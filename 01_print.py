263a8d07d7df6b3a395f791283081dc1ed63186e
Injustise <2029264452@qq.com>
2026-08-30 10:12:00 +0800

    8-30

--- 01_print.py
+++ 01_print.py
@@ -12,4 +12,9 @@ r = float(input("请输入圆的半径："))
 L = 2 * math.pi * r
 S = math.pi * r ** 2
 print("圆的周长：%.2f" % L)
-print("圆的面积：%.2f" % S)
+print("圆的面积：%.2f\n" % S)
+
+height = float(input("请输入您的身高（cm）："))
+weight = float(input("请输入您的体重（kg）："))
+BMI = weight / (height / 100) ** 2
+print(f"{BMI = :.2f}")
